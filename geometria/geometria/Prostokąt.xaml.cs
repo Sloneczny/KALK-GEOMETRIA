eaedcff898ade6d8f6b25fa2b2985dd26ce3a555
Your Name <sansong@o2.pl>
2022-10-02 11:33:54 +0200

    edit start

---
+++ geometria/geometria/Prostokąt.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace geometria
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Prostokąt.xaml
+    /// </summary>
+    public partial class Prostokąt : Window
+    {
+        public Prostokąt()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow w = new MainWindow();
+            this.Visibility = Visibility.Hidden;
+            w.Show();
+        }
+    }
+}
